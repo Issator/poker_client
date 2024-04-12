93a9b8463a3c327ef3482da29faa113a3983ac5d
Issator <jakubniemiec.poczta@gmail.com>
2024-04-12 18:05:49 +0200

    hand + board

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -4,16 +4,30 @@
 
 import CardIcon from "./CardIcon"
 
-export default function Card({value, sign, className}){
+export default function Card({value, sign, className = "", hidden=false}){
 
     const cardColor = (sign == 'H' || sign == 'D') ? 'danger' : 'dark'
     const icon = CardIcon({sign})
+
+    if(hidden){
+        return (
+            <div className={`play-card text-primary shadow me-2 ${className} bg-primary`}>
+                <div className="card-sign card-st ">
+                    0
+                </div>
+                <h1 className="card-sign display-6 fw-bold">0</h1>
+                <div className="card-sign card-be">
+                    0
+                </div>
+            </div>
+        )
+    }
     return (
-        <div className={`play-card text-${cardColor} shadow ${className}`}>
+        <div className={`play-card text-${cardColor} shadow me-2 ${className}`}>
             <div className="card-sign card-st">
                 {value} {icon}
             </div>
-            <h1 className="card-sign display-1 fw-bold">{icon}</h1>
+            <h1 className="card-sign display-6 fw-bold">{icon}</h1>
             <div className="card-sign card-be">
                 {value} {icon}
             </div>
--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -6,7 +6,7 @@ export default function GamePage(){
 
     const line = (sign) => {
         return (
-            <div className="d-flex flex-row justify-content-evenly mb-3" style={{width: '200rem'}}>
+            <div className="d-flex flex-row justify-content-evenly mb-3" style={{width: '60rem'}}>
                 {array.map(value => {
                     return <Card value={value} sign={sign}/>
                 })}
@@ -15,12 +15,49 @@ export default function GamePage(){
     }
 
     return(
-        <>
-        <h1>Game Page</h1>
-        {line("H")}
-        {line("S")}
-        {line("D")}
-        {line("C")}
-        </>
+        <div className="vh-100 p-5">
+            <div className="board h-100 position-relative">
+                <div className="top-hand w-100">
+                    <div className="hand">
+                        <Card value={12} sign={"D"}  hidden={true} />
+                        <Card value={8} sign={"C"}   hidden={true} />
+                        <Card value={2} sign={"H"}   hidden={true} />
+                        <Card value={"K"} sign={"D"} hidden={true} />
+                        <Card value={"J"} sign={"S"} hidden={true} />
+                    </div>
+                </div>
+
+                <div className="bottom-hand w-100">
+                    <div className="hand">
+                        <Card value={12} sign={"D"} />
+                        <Card value={8} sign={"C"}  />
+                        <Card value={2} sign={"H"}  />
+                        <Card value={"K"} sign={"D"}/>
+                        <Card value={"J"} sign={"S"}/>
+                    </div>
+                </div>
+
+                <div className="left-hand h-100 d-flex align-items-center">
+                    <div className="hand d-flex rotation-90">
+                        <Card value={12} sign={"D"}  hidden={true} />
+                        <Card value={8} sign={"C"}   hidden={true} />
+                        <Card value={2} sign={"H"}   hidden={true} />
+                        <Card value={"K"} sign={"D"} hidden={true} />
+                        <Card value={"J"} sign={"S"} hidden={true} />
+                    </div>
+                </div>
+
+                <div className="right-hand h-100 d-flex align-items-center">
+                    <div className="hand rotation-270">
+                        <Card value={12} sign={"D"}  hidden={true} />
+                        <Card value={8} sign={"C"}   hidden={true} />
+                        <Card value={2} sign={"H"}   hidden={true} />
+                        <Card value={"K"} sign={"D"} hidden={true} />
+                        <Card value={"J"} sign={"S"} hidden={true} />
+                    </div>
+                </div>
+            </div>
+        
+        </div>
     )
 }
