70afaec696cde2848e43054dae03e7e30586494d
Issator <jakubniemiec.poczta@gmail.com>
2024-05-28 22:20:50 +0200

    odwrotnie

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -184,8 +184,8 @@ export default function GamePage(){
         players[0].cards.forEach((card, id) => {
             if(selected[id]){
                 toChange.push({
-                    kolory: card.kolor,
-                    hierarchia: card.znak
+                    kolory: card.znak,
+                    hierarchia: card.kolor
                 })
             }
         })
